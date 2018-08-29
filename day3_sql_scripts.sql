38f81073b416af8fb615c83391158feacf99ad07
MontgomeryNugen <montgomery.nugen@gmail.com>
2018-08-29 11:56:18 -0400

    various sql scripts in class

---
+++ day3_sql_scripts.sql
@@ -0,0 +1,10 @@
+SELECT * -- ProductID, code, Description, ListPrice 
+  FROM product
+  WHERE ListPrice = 57.50
+  AND code = 'java';
+  
+select * from product
+ where ListPrice > 55;
+ 
+SELECT * FROM product
+ order by ListPrice desc;
